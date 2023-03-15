a7da9d53fb1aec31ee1cfe2adcd345921c18e0ff
alexsfking <104585968+alexsfking@users.noreply.github.com>
2023-03-15 12:31:30 +1100

    added a chat-gpt comment

--- loney_integer.ts
+++ loney_integer.ts
@@ -32,6 +32,29 @@ The unique element is 4.
 */
 
 /*
+***Chat-GPT***
+The lonelyinteger function takes an array of integers as input, where all
+elements appear twice except for one. The goal is to find and return the unique
+element. The function uses a hash table and a Set to accomplish this:
+
+    1. Create an empty Set called uniq and an empty hash table called hash.
+    2. Loop through the array a.
+    3. For each element a[i], check if it is in hash. If it is:
+        a. If a[i] is also in uniq, it means we have encountered the element 
+        twice. So we remove it from uniq.
+        b. If a[i] is not in hash, it means we have encountered it for the first
+        time. So we add it to both uniq and hash.
+    4. After the loop, there should be only one element left in uniq, which is 
+    the unique element we are looking for. We use the values method of the Set to
+    get an iterator over its values, and the next method of the iterator to get 
+    the first (and only) value.
+
+Finally, the main function reads the input from the standard input stream, calls
+the lonelyinteger function with the input array, and writes the result to the
+standard output stream.
+*/
+
+/*
  * Complete the 'lonelyinteger' function below.
  *
  * The function is expected to return an INTEGER.
