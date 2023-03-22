afa9dad4c64ed41f4bc9909adae990bf41a8b115
alexsfking <104585968+alexsfking@users.noreply.github.com>
2023-03-22 21:58:52 +1100

    added a chat-gpt comment

--- permuting_two_arrays.ts
+++ permuting_two_arrays.ts
@@ -29,6 +29,28 @@ for all i return "YES" else return "NO"
 */
 
 /*
+***Chat-GPT***
+The function twoArrays takes three arguments: an integer k, an array A of
+integers, and an array B of integers. It returns a string "YES" if the sum of
+the ith element in A and the ith element in B is greater than or equal to k for
+all i, and "NO" otherwise.
+
+In the function, the arrays A and B are sorted in ascending and descending order
+respectively using the sort() method. The function then loops through each
+element of the sorted arrays and checks if the sum of the corresponding elements
+is greater than or equal to k. If at any point the sum is less than k, the
+function returns "NO". If all the sums are greater than or equal to k, the
+function returns "YES".
+
+The main() function reads input from the standard input and writes the output to
+standard output. It first reads the number of test cases q from the input,
+followed by q sets of inputs each containing the length of the arrays n, the
+integer k, and the arrays A and B. For each set of inputs, the function calls
+twoArrays with the corresponding arguments and writes the result to standard
+output.
+*/
+
+/*
  * Complete the 'twoArrays' function below.
  *
  * The function is expected to return a STRING.
