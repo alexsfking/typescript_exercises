0c69f621f985a9d797f443082e8b55e75ee91f94
alexsfking <104585968+alexsfking@users.noreply.github.com>
2023-04-03 08:42:19 +1000

    added chat-gpt comment

--- sherlock_and_array.ts
+++ sherlock_and_array.ts
@@ -24,6 +24,50 @@ function readLine(): string {
 }
 
 /*
+Watson gives Sherlock an array of integers. His challenge is to find an element
+of the array such that the sum of all elements to the left is equal to the sum
+of all elements to the right.
+*/
+
+/*
+***Chat-GPT***
+This is TypeScript code for a function called balancedSums. This function takes
+an array of integers as an argument and returns a string. The goal of the
+function is to determine if there is an index where the sum of the elements on
+the left is equal to the sum of the elements on the right.
+
+The function begins with two helper functions: smallEdgeCases and
+headTailEdgeCases. These helper functions are used to handle edge cases where
+the array is either empty, has only one or two elements, or has zeros at the
+beginning or end of the array.
+
+The balancedSums function then checks if the length of the array is less than 3.
+If it is, it calls the smallEdgeCases function to handle the edge cases and
+returns "YES" or "NO" depending on whether the edge case is balanced.
+
+If the length of the array is 3 or greater, the function initializes two
+variables, left and right, to 0. It then calculates the sum of all the elements
+in the array and assigns it to right.
+
+The function then checks for head and tail edge cases by calling the
+headTailEdgeCases function. If the array has zeros at either the beginning or
+the end, the headTailEdgeCases function handles these cases and returns "YES" if
+they are balanced. 
+@@@This is false. It checks for [number,0,...,0] or [0,...,0,number]@@@
+
+If the array is not an edge case, the function enters a loop that iterates over
+all elements of the array except the first and last ones. Inside the loop, the
+function subtracts the current element from right and adds it to left. If left
+and right are equal, the function returns "YES".
+
+If the loop completes without finding a balanced index, the function returns
+"NO".
+
+The main function reads input from the user, calls the balancedSums function,
+and writes the result to the console.
+*/
+
+/*
  * Complete the 'balancedSums' function below.
  *
  * The function is expected to return a STRING.
