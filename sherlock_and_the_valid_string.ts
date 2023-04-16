7998ec78a0a3dbf78393c13a9ccae7ed749ab93b
alexsfking <104585968+alexsfking@users.noreply.github.com>
2023-04-16 14:33:13 +1000

    added a chat-gpt comment

--- sherlock_and_the_valid_string.ts
+++ sherlock_and_the_valid_string.ts
@@ -55,6 +55,47 @@ A single string s.
 */
 
 /*
+***Chat-GPT***
+This is a TypeScript code that defines a function isValid which takes a string s
+as input and returns either "YES" or "NO" depending on whether the input string
+is valid or not. The validity of the string is determined based on two
+conditions:
+
+    * If all the characters in the string have the same frequency, then the 
+    string is valid. 
+    * If we can remove only one character from the string and the remaining
+    characters will occur the same number of times, then the string is valid.
+
+The code first defines two helper functions, countFrequency and
+countFrequencyValues. countFrequency takes a string as input and returns a
+dictionary (Record) where the keys are the characters in the input string and
+the values are their frequency in the string. countFrequencyValues takes the
+frequency dictionary as input and returns another dictionary where the keys are
+the frequencies of the characters and the values are the number of characters
+that have that frequency.
+
+The isValid function uses these two helper functions to determine the validity
+of the input string. It first calculates the frequency dictionary of the input
+string using countFrequency, and then calculates the frequency value dictionary
+using countFrequencyValues. It then checks the following conditions:
+
+    * If there is only one key in the frequency value dictionary, then the 
+    string is valid.
+    * If there are two keys in the frequency value dictionary, then the string
+    is valid if either of the following conditions are met: 
+        * One of the keys has a frequency of 1.
+        * The difference between the two keys is 1 and one of the keys has
+        a frequency of 1. 
+    * If there are more than two keys in the frequency value
+    dictionary, then the string is not valid.
+
+The main function reads the input string from standard input, calls the isValid
+function to determine the validity of the input string, and then writes the
+result to standard output. The output is written to a file whose path is
+specified in the OUTPUT_PATH environment variable.
+*/
+
+/*
  * Complete the 'isValid' function below.
  *
  * The function is expected to return a STRING.
